c40e508c058224643ab2bb2100655fb0080268ad
aasim06 <aasimameer06@gmail.com>
2026-08-05 01:52:37 +0500

    Move root page to src/app/page.tsx for cPanel route resolution

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+import { Hero } from "@/components/home/hero";
+import { RecentSearches } from "@/components/home/recent-searches";
+import { PopularFlights } from "@/components/home/popular-flights";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="min-h-[calc(100vh-4rem)]">
+        <main>
+          <Hero />
+          <RecentSearches />
+          <PopularFlights />
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
