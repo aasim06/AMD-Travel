07151ce0fe6be33ef85d48ffca93c33efc489989
aasim06 <aasimameer06@gmail.com>
2026-08-05 02:13:42 +0500

    Force create root page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Hero } from "@/components/home/hero";
 import { RecentSearches } from "@/components/home/recent-searches";
 import { PopularFlights } from "@/components/home/popular-flights";
 
-
 export default function Home() {
   return (
     <>
